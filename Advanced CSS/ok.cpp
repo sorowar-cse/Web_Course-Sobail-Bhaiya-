3fe0b8e03fb05be572db6cb092bcb9ea4bfadb31
Sorowar Mahabub <sorowarmahabub.bsc.cse.iiuc@gmail.com>
2023-03-28 21:00:23 +0600

    Bot_02_03

---
+++ Advanced CSS/ok.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    for (int i = 24; i < 46; i++)
+    {
+        cout << "Day-" << i << "Live Link: https://d-" << i << ".netlify.app" << endl;
+        cout << "<hr>" << endl;
+    }
+    return 0;
+}
